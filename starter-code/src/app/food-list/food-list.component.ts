e6becdb9bec1f330c67b6dfc0ce3d96adabcd38f
Ana Raballo <anitaraballo@gmail.com>
2018-03-06 18:02:00 -0500

    Inital commit

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,36 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  boolean: boolean;
+  newFood: Object=[];
+  newList: Object[];
+
+  buttonClickMethod(){
+    if(this.boolean == true) {
+      this.boolean = false;
+    } else {
+      this.boolean = true;
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
+    this.boolean = true
+  }
+
+  anotherFood(){
+    console.log("Another food has been called");
+    this.foods.push(this.newFood);
+    this.newFood = {};
+  }
+
+  addList(){
+    console.log("Food")
+    this.foods.push(this.newList)
+    this.newList = [];
   }
 
 }
